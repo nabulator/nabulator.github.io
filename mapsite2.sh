8569f84020add7cacb1c207c3d5ca6cb12a6d3c4
czery <studioscws@gmail.com>
2021-03-23 09:50:39 -0400

    play with sitemap.xml scripts a bit

---
+++ mapsite2.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+# Invokes python script from https://github.com/c4software/python-sitemap
+
+/home/taeda/tools/python-sitemap/main.py --domain http://nabulator.com --images | xmllint --format - > /usr/share/nginx/html/static/sitemap.xml
+
